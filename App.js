650c941d6d5fcce9f7a1ad868a9def7cae9be055
abhi012323 <abhisingh.gc@gmail.com>
2024-12-19 15:39:41 +0530

    Initial commit with React Native app code

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import UserListScreen from './screens/UserListScreen';
+import UserDetailsScreen from './screens/UserDetailsScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="UserList" component={UserListScreen} options={{ title: 'User Directory' }} />
+        <Stack.Screen name="UserDetails" component={UserDetailsScreen} options={{ title: 'User Details' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
